719713085eb122cc721bd94b46c39baf0ed1bc55
Riley <riley.childs@yahoo.com>
2022-10-27 08:58:36 -0400

    Created layout for cart component

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,22 @@
 <script>
   import Nav from "./components/Nav.svelte";
+  import Cart from "./components/Cart.svelte";
 </script>
 
 <main>
   <section id="top-row">
     <Nav />
+	<Cart/>
   </section>
 </main>
 
 <style>
 	main{
 		margin: 2rem;
+	}
+	#top-row{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 	}
 </style>
---
+++ src/components/Cart.svelte
@@ -0,0 +1,15 @@
+<section class="cart">
+    <div class="cart__icon">
+        <img src="icon-cart.svg" alt="cart-icon">
+    </div>
+    <div class="user-avatar">
+        <img src="image-avatar.jpg" alt="user">
+    </div>
+</section>
+
+<style lang="scss">
+    .cart{
+        display: flex;
+        flex-direction: row;
+    }
+</style>
