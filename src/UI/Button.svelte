d35ed415470e685a49cfc0df6e76958ba8d841cd
Riley <riley.childs@yahoo.com>
2022-10-29 15:00:33 -0400

    Created univeral styles

--- src/UI/Button.svelte
+++ src/UI/Button.svelte
@@ -5,5 +5,16 @@
 <button on:click><slot /></button>
 
 <style lang="scss">
+    @import "../scss/global";
+button {
+  color: white;
+  background-color: $orange;
+  outline: none;
+  font-weight: 700;
+  &:hover{
+    background-color: $pale-orange;
+
+  }
+}
 
 </style>
