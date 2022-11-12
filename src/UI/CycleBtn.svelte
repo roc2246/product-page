a64abf0c6ab3a894980b83f8c7a6b1cb06e22f6b
Riley <riley.childs@yahoo.com>
2022-11-12 07:06:32 -0500

    Created UI for gallery cycle buttons

--- src/UI/CycleBtn.svelte
+++ src/UI/CycleBtn.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-  class="gallery__{direction}--{mode}"
+  class="gallery__{direction} gallery__{direction}--{mode}"
   on:keydown
   on:click={() => dispatch("cycle")}
 >
@@ -17,36 +17,61 @@
 <style lang="scss">
   @import "../scss/global";
   .gallery {
+    &__previous,
+    &__next {
+      position: absolute;
+
+      border-radius: 50%;
+      width: 2rem;
+      height: 2rem;
+      background-color: white;
+      padding: 0.5rem;
+
+      & > img {
+        position: relative;
+        top: 50%;
+        left: 25%;
+        transform: translate(0%, -50%);
+      }
+    }
+
     &__previous {
-      &--gallery, &--lightbox{
-      cursor: pointer;
+      &--gallery,
+      &--lightbox {
+        cursor: pointer;
       }
       &--gallery {
         display: none;
+        top: 35vh;
+        left: 15vw;
       }
     }
     &__next {
-      &--gallery, &--lightbox{
-      cursor: pointer;
+      &--gallery,
+      &--lightbox {
+        cursor: pointer;
       }
       &--gallery {
         display: none;
+        position: absolute;
+        top: 35vh;
+        left: 75vw;
       }
     }
   }
 
   @media (max-width: $mobile) {
     .gallery {
-    &__previous {
-      &--gallery {
-        display: inline-block;
+      &__previous {
+        &--gallery {
+          display: inline-block;
+        }
+      }
+      &__next {
+        &--gallery {
+          display: inline-block;
+        }
       }
     }
-    &__next {
-      &--gallery {
-        display: inline-block;
-      }
-    }
-  }
   }
 </style>
